d5b7f0bf31173d9b224c4ae1a3803c61afe63999
Marcel Lukas <m.lukas@mailbox.org>
2026-05-08 17:01:48 +0200

    feat(urls): serve media files through re_path configuration

--- core/urls.py
+++ core/urls.py
@@ -15,15 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('auth_app.api.urls')),
     path('api/', include('video_app.api.urls')),
     path('django-rq/', include('django_rq.urls')),
+    re_path(
+        r'^media/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.MEDIA_ROOT},
+    ),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
